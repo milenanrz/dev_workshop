7843c4a5742ecc63063a3ab682460b48fd298932
Milena Narvaez <narvaezmilena084@gmail.com>
2025-03-07 17:38:42 -0500

    data Completo

--- src/data/data.py
+++ src/data/data.py
@@ -34,6 +34,10 @@ class Data:
         Returns:
             int: Índice del elemento o -1 si no se encuentra
         """
+        for indice, valor in enumerate(lista): #obtiene tanto el indice como el elemento
+            if valor == elemento:
+                return indice
+        return -1
         pass
     
     def eliminar_duplicados(self, lista):
@@ -47,7 +51,14 @@ class Data:
         Returns:
             list: Lista sin elementos duplicados
         """
-    
+        listaSinDuplicados = []
+        elementosDuplicados =[]
+        for i in lista:
+            elemento = (i, type(i)) #obtiene el tipo de dato
+            if elemento not in elementosDuplicados:
+                elementosDuplicados.append(elemento)
+                listaSinDuplicados.append(i) #agrega elementos a la lista
+        return listaSinDuplicados
         pass
     
     def merge_ordenado(self, lista1, lista2):
@@ -61,7 +72,7 @@ class Data:
         Returns:
             list: Lista combinada y ordenada
         """
-        
+        return sorted(lista1 + lista2)
         pass
     
     def rotar_lista(self, lista, k):
@@ -75,6 +86,15 @@ class Data:
         Returns:
             list: Lista rotada
         """
+        if not lista:
+            return []
+
+        for i in range(k):
+            aux = lista[len(lista) - 1] #se guarda el ultimo elemento en aux
+            for j in range(len(lista) - 1, 0, -1):
+                lista[j] = lista[j - 1]
+            lista[0] = aux #elemento guardado en aux se pone en primera posicion de la lista
+        return lista    
         pass
     
     def encuentra_numero_faltante(self, lista):
@@ -87,6 +107,10 @@ class Data:
         Returns:
             int: El número que falta en la secuencia
         """
+        n = len(lista) + 1  # número total de elementos
+        sumaTotal = n * (n + 1) // 2  # suma de los primeros n números
+        sumaLista = sum(lista)
+        return sumaTotal - sumaLista
         pass
     
     def es_subconjunto(self, conjunto1, conjunto2):
@@ -100,6 +124,10 @@ class Data:
         Returns:
             bool: True si conjunto1 es subconjunto de conjunto2, False en caso contrario
         """
+        for i in conjunto1:
+            if i not in conjunto2:
+                return False
+        return True
         pass
     
     def implementar_pila(self):
@@ -109,6 +137,33 @@ class Data:
         Returns:
             dict: Diccionario con métodos push, pop, peek y is_empty
         """
+        pila = [] #Last In First Out
+
+        def push(elemento): #agrega elemento en la ultima posicion
+            pila.append(elemento)
+
+        def pop(): #elimina el ultimo elemento
+            if not is_empty():
+                return pila.pop()
+            else:
+                return None  
+
+        def peek(): #devuelve el elemento que esta en el tope
+            if not is_empty():
+                return pila[-1]
+            else:
+                return None  
+
+        def is_empty():
+            return len(pila) == 0
+
+        return {
+            "push": push,
+            "pop": pop,
+            "peek": peek,
+            "is_empty": is_empty,
+        }
+
         pass
     
     def implementar_cola(self):
@@ -118,6 +173,33 @@ class Data:
         Returns:
             dict: Diccionario con métodos enqueue, dequeue, peek y is_empty
         """
+        cola = [] #First In First Out
+
+        def enqueue(elemento): #añade un elemento al inicio
+            cola.append(elemento)
+        
+        def dequeue(): #elimina el primer elemento
+            if not len(cola) == 0:
+                return cola.pop(0)
+            else:
+                return None  
+
+        def peek(): #devuelve el primer elemento
+            if not len(cola) == 0:
+                return cola[0]
+            else:
+                return None 
+        
+        def is_empty():
+            return len(cola) == 0
+
+        return {
+            "enqueue": enqueue,
+            "dequeue": dequeue,
+            "peek": peek,
+            "is_empty": is_empty,
+        }
+
         pass
     
     def matriz_transpuesta(self, matriz):
@@ -130,4 +212,15 @@ class Data:
         Returns:
             list: Matriz transpuesta
         """
+        if not matriz or not matriz[0]:  # verifica si la matriz está vacía
+            return []
+
+        transpuesta = []
+
+        for i in range(len(matriz[0])):
+            transpuesta.append([])
+            for j in range(len(matriz)):
+                transpuesta[i].append(matriz[j][i])
+        return transpuesta        
+
         pass
